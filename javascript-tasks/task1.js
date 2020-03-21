1b5869210054dd7a24e7fc450656f09321165504
Isaac Sokari <isaacsokari@gmail.com>
2020-03-21 14:03:54 +0100

    added a comment at the end

--- javascript-tasks/task1.js
+++ javascript-tasks/task1.js
@@ -12,3 +12,5 @@ if (courses.length !== 0 && courses.length % 2 === 1) {
     console.log(i);
   }
 }
+
+/* I used console.log instead of return as a return statement would have involved pushing the values to a new array and returning values from that array and we weren't asked to do that. */
