a65a6f33bb66b049efc51823dbdf32d2cc233a04
Isaac Sokari <isaacsokari@gmail.com>
2020-03-21 14:16:38 +0100

    used an array to return numbers

--- javascript-tasks/task1.js
+++ javascript-tasks/task1.js
@@ -1,16 +1,22 @@
-// lastName, firstName middleName
+// order = lastName, firstName middleName
 let fullName = "Sokari, Isaac Tamunoipirinye";
 
 let courses = ["HTML", "CSS", "Javascript", "Design"];
 
-if (courses.length !== 0 && courses.length % 2 === 1) {
-  for (let i = 1; i <= 200; i += 2) {
-    console.log(i);
+let returnedNumbers = [];
+function evenOrOdd() {
+  if (courses.length !== 0 && courses.length % 2 === 1) {
+    for (let i = 1; i <= 200; i += 2) {
+      returnedNumbers.push(i);
+    }
+  } else if (courses.length !== 0 && courses.length % 2 === 0) {
+    for (let i = 0; i <= 200; i += 2) {
+      returnedNumbers.push(i);
+    }
   }
-} else if (courses.length !== 0 && courses.length % 2 === 0) {
-  for (let i = 0; i <= 200; i += 2) {
-    console.log(i);
-  }
+  return returnedNumbers;
 }
 
-/* I used console.log instead of return as a return statement would have involved pushing the values to a new array and returning values from that array and we weren't asked to do that. */
+console.log(evenOrOdd());
+
+/* Chose to return an array to save space on the console. */
